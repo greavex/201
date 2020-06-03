eb0ba615928e314fcdcefec5e8649fe4d385a3c8
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-06-03 15:50:34 +0800

    add

---
+++ code/0603/16.計時器class元件與functional元件-範例樣本/components/MyTimerClass.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class MyTimerClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return <></>
+  }
+}
+
+export default MyTimerClass
