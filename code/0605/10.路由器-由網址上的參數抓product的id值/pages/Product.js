075d40f95d9df561c61ec830a0ed8816d3084bce
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-06-05 15:54:26 +0800

    add

---
+++ code/0605/10.路由器-由網址上的參數抓product的id值/pages/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+import MyBanner from '../components/MyBanner'
+
+function Product(props) {
+  console.log(props)
+  return (
+    <>
+      <MyBanner title="產品" lead="product" />
+      <h2>目前的產品編號是(從網址得上得到)：{props.match.params.id}</h2>
+    </>
+  )
+}
+
+// 高階元件的用法
+export default withRouter(Product)
