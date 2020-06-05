b0c071ebf212f346fef3371669a9b78578242c4c
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-06-05 11:02:45 +0800

    Update MainContent.js

--- code/0605/4.Todo程式分拆-MainContent與TodoList/components/MainContent.js
+++ code/0605/4.Todo程式分拆-MainContent與TodoList/components/MainContent.js
@@ -7,8 +7,10 @@ function MainContent(props) {
     <>
       <main role="main" className="flex-shrink-0">
         <div className="container">
+          {/* 網頁的主內容標題 */}
           <h1 className="mt-5">{props.title}</h1>
           <hr />
+          {/* 主要內容寫在這 */}
           {props.children}
         </div>
       </main>
