aefd922160d7ecd810abfeb8c3ef9aabe13f9960
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-06-08 09:49:58 +0800

    ADD

--- code/0608/1.querystring查詢字串範例/pages/Product.js
+++ code/0608/1.querystring查詢字串範例/pages/Product.js
@@ -7,12 +7,16 @@ import MyBreadcrumb from '../components/MyBreadcrumb'
 function Product(props) {
   console.log(props)
 
+  // 查詢字串可由props.location.search得到
+  // 使用URLSearchParams來準備解析得到的查詢字串
+  // URLSearchParams注意ie相容性問題
   const searchParams = new URLSearchParams(props.location.search)
 
   return (
     <>
       <MyBanner title="產品" lead="product" />
       <MyBreadcrumb />
+      {/* 用get方法即可解析出查詢字 */}
       <h2>目前的產品類型是(從網址得上得到)：{searchParams.get('cat')}</h2>
       <h2>目前的產品編號是(從網址得上得到)：{searchParams.get('pid')}</h2>
     </>
