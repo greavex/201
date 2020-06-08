ad6c8a95c3dc3e15f5a48bad9333a266dd60f74c
Anna <55761750+Pull-Plus@users.noreply.github.com>
2020-06-08 16:50:40 +0800

    ADD

---
+++ code/0608/13.0608所有專案檔案/src/components/MyNavbar.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import { Link, NavLink, withRouter } from 'react-router-dom'
+
+function MyNavbar(props) {
+  const { auth, name } = props
+
+  const loginButton = (
+    <>
+      <Button
+        variant="outline-light"
+        onClick={() => {
+          props.history.push('/memberlogin')
+        }}
+      >
+        登入
+      </Button>
+    </>
+  )
+  const logoutButton = (
+    <>
+      <span style={{ color: '#ffffff' }}>{name}, 你好</span>
+      <Button
+        variant="outline-light"
+        onClick={() => {
+          props.history.push('/memberlogin')
+        }}
+      >
+        登出
+      </Button>
+    </>
+  )
+
+  const displayButton = auth ? logoutButton : loginButton
+
+  return (
+    <>
+      <Navbar bg="primary" variant="dark" fixed="top">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          {/* 把Nav.Link當作NavLink來使用 */}
+          {/* 記得首頁`/`要加exact作精確比對，不然都會一直點亮 */}
+          <Nav.Link as={NavLink} to="/" exact>
+            Home
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/about">
+            關於我們
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/counter">
+            計數器
+          </Nav.Link>
+          {auth ? (
+            <Nav.Link as={NavLink} to="/todoapp">
+              待辨事項
+            </Nav.Link>
+          ) : (
+            ''
+          )}
+          <Nav.Link as={NavLink} to="/product">
+            產品
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/memberlogin">
+            會員登入
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/productlist">
+            產品列表
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/cart">
+            購物車
+          </Nav.Link>
+        </Nav>
+        <Form inline>{displayButton}</Form>
+      </Navbar>
+    </>
+  )
+}
+
+export default withRouter(MyNavbar)
